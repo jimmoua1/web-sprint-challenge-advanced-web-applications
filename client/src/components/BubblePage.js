911b06a69e93c1d8d91e64d207c371e295cfa07c
jimmoua1 <tsheejmaus@yahoo.com>
2021-01-24 13:02:11 -0800

    added useEffect

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -9,6 +9,13 @@ const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`http://localhost:5000/api/colors`)
+      .then(res => setColorList(res.data))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
